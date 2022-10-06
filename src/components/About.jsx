675e6cec0be5f5a9db7202a7dad6f9d69f9dce6e
NguyenHoangDuy <19521425@gm.uit.edu.vn>
2022-10-06 11:16:53 +0700

    change commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
     return (
         <div>
             <div className="container py-5 my-5">
-                <div className="row ">
+                <div className="row">
                     <div className="col-md-6">
                         <h1 className='text-warning fw-bold mb-4'>ABOUT US</h1>
                         <p className="lead mb-4 fs-5">Dear customers, JSC TM-DV-SX GNAHYUD (GNAHYUD HD) was established in 2022 with the main product is the guitar, after nearly 1 year of operation, the company has a relationship. Trade with famous musical instrument brands in the world such as Germany, France, the Netherlands, Italy, USA, UK, Japan, Taiwan, China, Korea, etc. Product categories are also expanded. richer and more diversified with musical instruments imported from the above countries such as acoustic pianos, electronic pianos, organs, guitars, drums, trumpets, other audio equipment and music accessories. Up to now, after many years of operation, with a staff, staff, enthusiastic, highly qualified, GnahYuD Company has been trusted by customers and has become one of the largest musical instruments in Ho Chi Minh City. Chi Minh.
